4bb538a981b6c43ff90ea3a9a085841dc334445b
Rest133 <31849309+Rest133@users.noreply.github.com>
2017-10-22 22:31:31 +0300

    Update Simple.kt

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -60,7 +60,7 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = (hours*3600)+(minutes
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = ((sagenes*48)+(arshins*16)+(vershoks))*4.445/100;
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes*48+arshins*16+vershoks)*4.445/100
 
 /**
  * Тривиальная
@@ -68,7 +68,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = ((sagene
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (grad + min/60.0+ sec/3600.0)*PI/180 ;
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (grad+min/60.0+sec/3600.0)*PI/180
 
 /**
  * Тривиальная
@@ -76,7 +76,7 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (grad + min/60.0+ sec
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = pow((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1),1.0/2);
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(pow((x2-x1),2.0)+pow((y2-y1),2.0))
 
 /**
  * Простая
